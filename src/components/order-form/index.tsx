8db1133ae0852a27a55c68c25dda0724bc5eaf4e
yasuro <dev@csproper.co.jp>
2026-02-11 22:32:28 +0900

    離脱防止機能を修正

--- src/components/order-form/index.tsx
+++ src/components/order-form/index.tsx
@@ -232,6 +232,7 @@ export function OrderForm({
 
     const handler = (e: BeforeUnloadEvent) => {
       e.preventDefault();
+      e.returnValue = "";
     };
     window.addEventListener("beforeunload", handler);
     return () => window.removeEventListener("beforeunload", handler);
